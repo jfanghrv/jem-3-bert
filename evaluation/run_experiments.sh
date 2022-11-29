a9dc94545cd77e46a9b42da158375e2e60ff7c5d
Mengyuan Li <Moly.LMY@outlook.com>
2022-11-29 18:30:34 -0500

    Add bash script to run experiments automatically

--- evaluation/run_experiments.sh
+++ evaluation/run_experiments.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-python3 evaluation.py --gold input/spider_test_gold.txt --pred input/spider_test_predict.txt --db database --table tables.json --etype all --output all
-python3 evaluation.py --gold input/spider_test_75pct_thresh_gold.txt --pred input/spider_test_75pct_thresh_predict.txt --db database --table tables.json --etype all --output max_75pct
-python3 evaluation.py --gold input/spider_test_l2norm_75pct_thresh_gold.txt --pred input/spider_test_l2norm_75pct_thresh_predict.txt --db database --table tables.json --etype all --output l2norm_75pct
+for file in ./input/*_gold.txt
+do
+    name=${file:8}
+    prefix=${name%_gold.txt}
+    python3 evaluation.py --gold ./input/${prefix}_gold.txt --pred ./input/${prefix}_predict.txt --db database --table tables.json --etype all --output ${prefix}
+done
